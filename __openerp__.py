85570d04344689c25f20f34b1abb5e9262d64be8
Alkivi <admin@alkivi.fr>
2014-02-20 13:47:59 +0100

    added security : need to be at least accountant

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 { 
    'name': 'Linxo', 
-   'version': '0.8.7', 
+   'version': '1.0.0', 
    'summary': 'Bank reconciliation using Linxo',
    'description': """
 Import bank statement from Linxo and perform automatic reconciliation.
